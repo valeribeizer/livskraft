4c85b471789a1c78036ee9406225634ae7623c07
Valeryia Beizer <strelcova.lera94@gmail.com>
2024-12-31 12:35:15 +0300

    small fixes version 3

--- src/SuccessMsg/SuccessMsg.js
+++ src/SuccessMsg/SuccessMsg.js
@@ -10,7 +10,15 @@ const SuccessMsg = ({ onClose }) => {
 
   return (
     <div className="overlay" onClick={handleOverlayClick}>
-      <div className="scs-msg-container">
+      <div
+        className="scs-msg-container"
+        style={{
+          backgroundImage:
+            window.innerWidth > 560
+              ? "url('/tsInfo-bg.png')"
+              : "url('/tsInfo-bg-mb.png')",
+        }}
+      >
         <h2 className="h2-scs">
           Tack för ditt förtroende! Jag återkommer så snart som möjligt för att
           hjälpa dig vidare!
