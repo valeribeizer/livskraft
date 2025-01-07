e21b58037ce30ff0ef71cb55b1d8bfd956b572fe
Valeryia Beizer <strelcova.lera94@gmail.com>
2025-01-07 14:24:39 +0300

    changed fa stod form and fixed slider

--- src/HomeScreen/HomeScreen.js
+++ src/HomeScreen/HomeScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import NavBar from "../NavBar/NavBar";
 import "./style.css";
 // import ContactForm from "../СontactForm/ContactForm";
