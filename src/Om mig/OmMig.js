01c6302b98aa2c2d15191b120d095d6ccfbe57bb
Valeryia Beizer <strelcova.lera94@gmail.com>
2025-01-10 18:54:59 +0300

    changes to om mig component

--- src/Om mig/OmMig.js
+++ src/Om mig/OmMig.js
@@ -11,7 +11,7 @@ const OmMig = () => {
       <div className="om-container">
         <h1>PERSONEN BAKOM LIVSKRAFT</h1>
         <div className="row row-custom">
-          <div className="col-12 col-md-6 col-lg-6">
+          <div className="col-12 col-md-6 col-lg-8">
             <p className="p-om">
               Hälsa är en av livets största gåvor – och mysterier. Ibland kan vi
               påverka den genom våra val, ibland är sjukdom bortom vår kontroll.
@@ -24,9 +24,21 @@ const OmMig = () => {
               med långa medicinlistor och små, men viktiga, frågor. Jag såg hur
               vården ofta saknade tid för det viktigaste – samtalet. Där föddes
               idén till Livskraft.
+              <br />
+              <br />
+              Idag arbetar jag med kliniska prövningar på ett läkemedelsföretag
+              och har fått en ännu djupare insikt i läkemedlens möjligheter och
+              begränsningar. Men det som driver mig är önskan att hjälpa på ett
+              mer hållbart och meningsfullt sätt. Livskraft är mitt sätt att
+              förena vetenskap och natur – att se hela människan och inte bara
+              symtomen. Som mamma till en tvååring vet jag hur viktigt det är
+              att hitta balans i livet och skapa utrymme för det som verkligen
+              betyder något. Den energin vill jag dela med dig här på Livskraft.
+              Min vision är att hjälpa dig hitta din väg till färre läkemedel,
+              mer välmående och en hälsa som håller – för livet.
             </p>
           </div>
-          <div className="col-12 col-md-6 col-lg-6 col-om-mig">
+          <div className="col-12 col-md-6 col-lg-4 col-om-mig">
             <img
               className="om-mig-img-1"
               src="om-mig-img-1.png"
@@ -35,89 +47,27 @@ const OmMig = () => {
             />
           </div>
         </div>
-        {window.innerWidth > 560 ? (
-          <div className="row row-custom custom-marg">
-            <div className="col-12 col-md-4 col-lg-4">
-              <img
-                className="om-mig-img"
-                src="om-mig-img.png"
-                alt="om-mig-img"
-                loading="lazy"
-              />
-            </div>
-            <div className="col-12 col-md-8 col-lg-8">
-              <p className="p-om">
-                Idag arbetar jag med kliniska prövningar på ett
-                läkemedelsföretag och har fått en ännu djupare insikt i
-                läkemedlens möjligheter och begränsningar. Men det som driver
-                mig är önskan att hjälpa på ett mer hållbart och meningsfullt
-                sätt. Livskraft är mitt sätt att förena vetenskap och natur –
-                att se hela människan och inte bara symtomen. Som mamma till en
-                tvååring vet jag hur viktigt det är att hitta balans i livet och
-                skapa utrymme för det som verkligen betyder något. Den energin
-                vill jag dela med dig här på Livskraft. Min vision är att hjälpa
-                dig hitta din väg till färre läkemedel, mer välmående och en
-                hälsa som håller – för livet.
-                <br />
-                <br />
-                Min vision med Livskraft är att göra hälsan både förståelig och
-                tillgänglig. Genom att kombinera min expertis som apotekare med
-                en holistisk syn på välmående, erbjuder jag vägledning för att
-                hjälpa dig navigera dina läkemedel, utforska naturliga lösningar
-                och skapa en hållbar hälsa som håller över tid.
-                <br />
-                <br />
-                Livskraft handlar om att se hela människan – dina mål,
-                utmaningar och möjligheter – och ge dig verktygen att ta
-                kontroll över din egen hälsoresa.
-              </p>
-              <p className="p1-om">
-                Utforska mina <Link to="/tjanster" className="om-link">tjänster</Link> här och ta första steget mot en hållbar hälsa.
-              </p>
-            </div>
-          </div>
-        ) : (
-          <div className="row row-custom custom-marg">
-            <div className="col-12 col-lg-8">
-              <p className="p-om">
-                Idag arbetar jag med kliniska prövningar på ett
-                läkemedelsföretag och har fått en ännu djupare insikt i
-                läkemedlens möjligheter och begränsningar. Men det som driver
-                mig är önskan att hjälpa på ett mer hållbart och meningsfullt
-                sätt. Livskraft är mitt sätt att förena vetenskap och natur –
-                att se hela människan och inte bara symtomen. Som mamma till en
-                tvååring vet jag hur viktigt det är att hitta balans i livet och
-                skapa utrymme för det som verkligen betyder något. Den energin
-                vill jag dela med dig här på Livskraft. Min vision är att hjälpa
-                dig hitta din väg till färre läkemedel, mer välmående och en
-                hälsa som håller – för livet.
-                <br />
-                <br />
-                Min vision med Livskraft är att göra hälsan både förståelig och
-                tillgänglig. Genom att kombinera min expertis som apotekare med
-                en holistisk syn på välmående, erbjuder jag vägledning för att
-                hjälpa dig navigera dina läkemedel, utforska naturliga lösningar
-                och skapa en hållbar hälsa som håller över tid.
-                <br />
-                <br />
-                Livskraft handlar om att se hela människan – dina mål,
-                utmaningar och möjligheter – och ge dig verktygen att ta
-                kontroll över din egen hälsoresa.
-              </p>
-              <p className="p1-om">
-                Utforska mina <Link to="/tjanster" className="om-link">tjänster</Link> här och ta första steget mot en hållbar hälsa.
-              </p>
-            </div>
-            <div className="col-12 col-lg-4 col-om-mig-1">
-              <img
-                className="om-mig-img"
-                src="om-mig-img.png"
-                alt="om-mig-img"
-                loading="lazy"
-              />
-            </div>
-          </div>
-        )}
+        <div className="row row-custom custom-marg">
+          <p className="p-om">
+            Min vision med Livskraft är att göra hälsan både förståelig och
+            tillgänglig. Genom att kombinera min expertis som apotekare med en
+            holistisk syn på välmående, erbjuder jag vägledning för att hjälpa
+            dig navigera dina läkemedel, utforska naturliga lösningar och skapa
+            en hållbar hälsa som håller över tid.
+            <br />
+            <br />
+            Livskraft handlar om att se hela människan – dina mål, utmaningar
+            och möjligheter – och ge dig verktygen att ta kontroll över din egen
+            hälsoresa.
+          </p>
+          <p className="p1-om">
+            Utforska mina{" "}
+            <Link to="/tjanster" className="om-link">
+              tjänster
+            </Link>{" "}
+            här och ta första steget mot en hållbar hälsa.
+          </p>
+        </div>
       </div>
       <Footer />
     </div>
