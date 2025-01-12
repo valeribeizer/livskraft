e98541c9c01bc0d2dcaa94b1633e88a43f4c69c5
Valeryia Beizer <strelcova.lera94@gmail.com>
2025-01-12 11:23:06 +0300

    deleted unness comments

--- src/SuccessMsg/SuccessMsg.js
+++ src/SuccessMsg/SuccessMsg.js
@@ -3,33 +3,6 @@ import "./style.css";
 import NavBar from "../NavBar/NavBar";
 import Footer from "../Footer/Footer";
 
-// const SuccessMsg = ({ onClose }) => {
-//   const handleOverlayClick = (e) => {
-//     if (e.target === e.currentTarget) {
-//       onClose();
-//     }
-//   };
-
-//   return (
-//     <div className="overlay" onClick={handleOverlayClick}>
-//       <div
-//         className="scs-msg-container"
-//         style={{
-//           backgroundImage:
-//             window.innerWidth > 560
-//               ? "url('/tsInfo-bg.png')"
-//               : "url('/tsInfo-bg-mb.png')",
-//         }}
-//       >
-//         <h2 className="h2-scs">
-//           Tack för ditt förtroende! Jag återkommer så snart som möjligt för att
-//           hjälpa dig vidare!
-//         </h2>
-//       </div>
-//     </div>
-//   );
-// };
-
 const SuccessMsg = () => {
   return (
     <div>
