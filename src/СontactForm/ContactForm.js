51f71ae55cadb452a34004ab625035c84237607b
Valeryia Beizer <strelcova.lera94@gmail.com>
2025-04-08 10:48:22 +0400

    fixed datetimepicker

--- src/СontactForm/ContactForm.js
+++ src/СontactForm/ContactForm.js
@@ -1,10 +1,10 @@
 import React, { useRef, useState } from "react";
-import { useLocation } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 import emailjs from "@emailjs/browser";
 import "./style.css";
 import NavBar from "../NavBar/NavBar";
 import Footer from "../Footer/Footer";
-import { Link } from "react-router";
+// import { Link } from "react-router";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { sv } from "date-fns/locale";
@@ -12,11 +12,16 @@ import { sv } from "date-fns/locale";
 const ContactForm = () => {
   const form = useRef();
   const location = useLocation();
+  const navigate = useNavigate();
   const source = location.state?.source;
   const [selectedDate, setSelectedDate] = useState(null);
 
   const sendEmail = (e) => {
     e.preventDefault();
+
+    // Debug
+    console.log("Sending time:", selectedDate?.toISOString());
+    console.log("Form time input value:", form.current["time"].value);
 
     emailjs
       .sendForm(
@@ -28,6 +33,7 @@ const ContactForm = () => {
       .then(
         () => {
           console.log("SUCCESS!");
+          navigate("/skickad");
         },
         (error) => {
           console.log("FAILED...", error.text);
@@ -51,7 +57,7 @@ const ContactForm = () => {
           personligen.
         </p>
         <form className="needs-validation" ref={form} onSubmit={sendEmail}>
-          <label for="validation-1" className="form-label">
+          <label htmlFor="validation-1" className="form-label">
             Namn och efternamn
           </label>
           <input
@@ -61,7 +67,7 @@ const ContactForm = () => {
             id="validation-1"
             required
           />
-          <label for="validation-2" className="form-label">
+          <label htmlFor="validation-2" className="form-label">
             E-post
           </label>
           <input
@@ -71,7 +77,7 @@ const ContactForm = () => {
             id="validation-2"
             required
           />
-          <label for="validation-3" className="form-label">
+          <label htmlFor="validation-3" className="form-label">
             Telefonnummer
           </label>
           <input
@@ -81,7 +87,7 @@ const ContactForm = () => {
             id="validation-3"
             required
           />
-          <label for="validation-4" className="form-label">
+          <label htmlFor="validation-4" className="form-label">
             När vill du bli kontaktad?
           </label>
           <DatePicker
@@ -93,21 +99,27 @@ const ContactForm = () => {
             dateFormat="Pp"
             locale={sv}
             minDate={new Date()}
-            minTime={
-              selectedDate &&
-              selectedDate.toDateString() === new Date().toDateString()
-                ? new Date()
-                : new Date().setHours(0, 0, 0, 0)
-            }
-            maxTime={new Date().setHours(23, 59, 0, 0)}
+            // minTime={
+            //   selectedDate &&
+            //   selectedDate.toDateString() === new Date().toDateString()
+            //     ? new Date()
+            //     : new Date().setHours(0, 0, 0, 0)
+            // }
+            // maxTime={new Date().setHours(23, 59, 0, 0)}
             required
           />
           <input
             type="hidden"
             name="time"
-            value={selectedDate ? selectedDate.toISOString() : ""}
+            value={
+              selectedDate
+                ? selectedDate.toLocaleString("sv-SE", {
+                    timeZone: "Europe/Stockholm",
+                  })
+                : ""
+            }
           />
-          <label for="validation-5" className="form-label">
+          <label htmlFor="validation-5" className="form-label">
             Hur föredrar du att bli kontaktad?
           </label>
           <select
@@ -116,14 +128,14 @@ const ContactForm = () => {
             name="metod"
             required
           >
-            <option selected disabled value="" style={{ fontSize: "12px" }}>
+            <option value="" disabled selected>
               Välj...
             </option>
             <option>SMS</option>
             <option>E-post</option>
             <option>Samtal</option>
           </select>
-          <label for="validation-6" className="form-label">
+          <label htmlFor="validation-6" className="form-label">
             Vad kan jag hjälpa dig med?
           </label>
           <textarea
@@ -131,27 +143,24 @@ const ContactForm = () => {
             id="validation-6"
             name="msg"
             style={{ height: "90px" }}
-            placeholder="Beskriv kort vad du behöver hjälp med eller vad du vill veta mer om. Ju mer jag vet, desto bättre kan jag förbereda mig för att hjälpa dig."
+            placeholder="Beskriv kort vad du behöver hjälp med eller vad du vill veta mer om..."
             required
           ></textarea>
           <div className="form-check">
             <input
-              class="form-check-input"
+              className="form-check-input"
               type="checkbox"
               value=""
               id="invalidCheck"
               required
             />
-            <label class="form-check-label" for="invalidCheck">
+            <label className="form-check-label" htmlFor="invalidCheck">
               Jag samtycker till att mina uppgifter används för att ge
               personligt anpassat stöd.
             </label>
           </div>
-          <button className="btn_main" type="submit">
-            <Link to="/skickad" className="btn-link">
-              {" "}
-              SKICKA
-            </Link>
+          <button className="btn_main btn_link" type="submit">
+            SKICKA
           </button>
         </form>
       </div>
